217c614c58315735974e26ec7e800e6f9a877318
mquintiero <manu_quintiero@hotmail.com>
2024-12-04 23:14:18 -0300

    Finish day 2

---
+++ day2/part1/main.py
@@ -0,0 +1,38 @@
+reports = []
+safeCount = 0
+
+with open('day2/input.txt', 'r') as file:
+    for line in file:
+        numbers = [int(s) for s in line.split() if s.isdigit()]
+        reports.append(numbers)
+
+def allIncreasing(report):
+    res = True
+    for i in range(len(report) - 1):
+        res = res and report[i] < report[i+1]
+    return res
+
+def allDecreasing(report):
+    res = True
+    for i in range(len(report) - 1):
+        res = res and report[i] > report[i+1]
+    return res
+
+def validDiffer(level1, level2):
+    differ = abs(level1 - level2)
+    return differ >= 1 and differ <= 3
+
+def validDifferings(report):
+    res = True
+    for i in range(len(report) - 1):
+        res = res and validDiffer(report[i], report[i+1])
+    return res    
+
+def isValidReport (report):
+    return (allIncreasing(report) or allDecreasing(report)) and validDifferings(report)
+
+for report in reports:
+    if (isValidReport(report)):
+        safeCount += 1
+
+print(safeCount)
