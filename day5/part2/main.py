060054ef0f850a747977c7337deccad79b6ecb93
mquintiero <manu_quintiero@hotmail.com>
2024-12-08 15:15:12 -0300

    Optimize day5 part2

--- day5/part2/main.py
+++ day5/part2/main.py
@@ -1,40 +1,21 @@
-import re
-"Very inefficient solution but it works... Took around 6 minutes to print result"
-content = []
+import functools
+
 sum = 0
-numbersAfterPage = dict()
 
-with open('day5/input.txt', 'r') as file:
-    for line in file:
-        content.append(line.rstrip("\n"))
+with open("day5/input.txt") as f:
+    ordering_rules, updates = f.read().split("\n\n")
+    ordering_rules = [tuple(map(int, rule.split("|"))) for rule in ordering_rules.split("\n")]
+    updates = [list(map(int, update.split(","))) for update in updates.split("\n")]
 
-splitIndex = content.index('')
-orderRules = content[:splitIndex]
-updates = content[splitIndex+1:]
-
-for rule in orderRules:
-    tempRule = rule.split("|")
-    numbersAfterPage.setdefault(tempRule[0], [])
-    numbersAfterPage[tempRule[0]].append(tempRule[1])
+def compare(a, b):
+    if (a, b) in ordering_rules: return -1
+    elif (b, a) in ordering_rules: return 1
+    else: return 0
 
 for update in updates:
-    correctPage = True
-    pages = re.findall(r'\d+',update)
-    i = 0
-    while i < len(pages):
-        j = 0
-        while j < len(pages):
-            if (i < j):
-                if (pages[i] in numbersAfterPage[pages[j]]):
-                    correctPage = False
-                    pages.insert(j+1, pages[i])
-                    pages.pop(i)
-                    i = j = 0
-                    continue
-            j += 1
-        i += 1
-    if (not correctPage):
-        sum = sum + int(pages[int(len(pages)/2)])
+    new = sorted(update, key=functools.cmp_to_key(compare))
+    if (new != update):
+      sum += int(new[len(new) // 2])
 
 
 print(sum)
