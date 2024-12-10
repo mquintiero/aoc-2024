4022284df299245eff0ab21830ed8f86577f92f5
mquintiero <manu_quintiero@hotmail.com>
2024-12-10 12:47:32 -0300

    Finish day 10

---
+++ day10/part2.py
@@ -0,0 +1,27 @@
+with open('day10/input.txt', 'r') as f:
+    lines = f.read().splitlines()
+
+grid = {}
+for y, line in enumerate(lines):
+    for x, char in enumerate(line):
+        grid[x + y * 1j] = int(char)
+
+trailheads = [key for key, value in grid.items() if value == 0]
+
+score = 0
+for trailhead in trailheads:
+    queue = [trailhead]
+    encountered_positions = set()
+    while len(queue) > 0:
+        position = queue.pop()
+        if position in encountered_positions:
+            continue
+        elevation = grid[position]
+        if (elevation == 9):
+            score += 1
+            continue
+        surrounding_positions = [position + direction for direction in [1, -1, 1j, -1j] 
+                                if position + direction in grid and grid[position + direction] == elevation + 1]
+        queue.extend(surrounding_positions)
+
+print(score)
